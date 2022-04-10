81b4d409168b11f0f696b2ee5a63f4cb33d03ab5
claudiunicolaa <claudiunicola96@gmail.com>
2022-04-10 22:17:06 +0300

    wip changes. not tested yet.

--- src/main/java/claudiunicolaa/keycloak/authenticator/MultipleEnrollmentTwoFactorAuthenticator.java
+++ src/main/java/claudiunicolaa/keycloak/authenticator/MultipleEnrollmentTwoFactorAuthenticator.java
@@ -1,9 +1,47 @@
 package claudiunicolaa.keycloak.authenticator;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.AuthenticationFlowError;
 import org.keycloak.authentication.Authenticator;
 import org.keycloak.models.*;
 
+import javax.ws.rs.core.Response;
+import java.util.Arrays;
+import java.util.List;
+
+
+//@JsonIgnoreProperties(value = { "required" })
+class TwoFactorAuthAttribute {
+	String type;
+	Boolean required;
+
+	public void setRequired(Boolean required) {
+		this.required = required;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	/**
+	 * Check if the two factor enrollment is needed.
+	 *
+	 * @return true if required=true and type is blank
+	 */
+	public Boolean enrollmentNeedsToBeMade() {
+		return required && type.isBlank();
+	}
+
+	public static TwoFactorAuthAttribute FromRawJSON(String rawJson) throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.readValue(
+			rawJson,
+			TwoFactorAuthAttribute.class
+		);
+	}
+}
 
 /**
  * @author Claudiu Nicola, https://claudiunicola.xyz, @claudiunicolaa
@@ -12,14 +50,51 @@ public class MultipleEnrollmentTwoFactorAuthenticator implements Authenticator {
 
 	private static final String TPL_CODE = "multiple-enrollment.ftl";
 
+	private static final List<String> AVAILABLE_METHODS = Arrays.asList("sms", "app");
+
 	@Override
 	public void authenticate(AuthenticationFlowContext context) {
-		// @todo
+//		KeycloakSession session = context.getSession();
+		UserModel user = context.getUser();
+		String twoFactorAuthAttrRaw =  user.getFirstAttribute("two_factor_auth");
+
+		try {
+			TwoFactorAuthAttribute userAttribute = TwoFactorAuthAttribute.FromRawJSON(twoFactorAuthAttrRaw);
+			// the two-factor authentication multiple enrollment form needs to be shown if the user has not enrolled yet
+			if (userAttribute.enrollmentNeedsToBeMade()) {
+				context.challenge(context.form().setAttribute("realm", context.getRealm()).createForm(TPL_CODE));
+				return;
+			}
+
+			// skip the authenticator if above conditions are not met
+			context.success();
+		} catch (Exception e) {
+			context.failureChallenge(AuthenticationFlowError.INTERNAL_ERROR,
+				context.form().setError("error", e.getMessage())
+					.createErrorPage(Response.Status.INTERNAL_SERVER_ERROR));
+		}
 	}
 
 	@Override
 	public void action(AuthenticationFlowContext context) {
-		// @todo
+		String chosenMethod = context.getHttpRequest().getDecodedFormParameters().getFirst("method");
+		if (chosenMethod == null) {
+			context.failureChallenge(AuthenticationFlowError.INTERNAL_ERROR,
+				context.form().createErrorPage(Response.Status.INTERNAL_SERVER_ERROR));
+			return;
+		}
+
+		if (!AVAILABLE_METHODS.contains(chosenMethod)) {
+			context.failureChallenge(AuthenticationFlowError.INTERNAL_ERROR,
+				context.form().createErrorPage(Response.Status.INTERNAL_SERVER_ERROR));
+			return;
+		}
+
+		// @todo: save the chosen method in the user's attributes
+		UserModel user = context.getUser();
+		user.setAttribute("me", List.of(chosenMethod));
+
+		context.success();
 	}
 
 	@Override
@@ -29,7 +104,7 @@ public class MultipleEnrollmentTwoFactorAuthenticator implements Authenticator {
 
 	@Override
 	public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
-		return true;
+		return user.getFirstAttribute("two_factor_auth") != null;
 	}
 
 	@Override
--- src/main/resources/theme-resources/templates/multiple-enrollment.ftl
+++ src/main/resources/theme-resources/templates/multiple-enrollment.ftl
@@ -1,1 +1,35 @@
 <#import "template.ftl" as layout>
+<#if section = "header">
+		${msg("multipleEnrollmentAuthTitle",realm.displayName)}
+	<#elseif section = "form">
+		<form id="kc-mutiple-enrollment-two-factor-auth-form" class="${properties.kcFormClass!}" action="${url.loginAction}" method="post">
+			<div class="${properties.kcFormGroupClass!}">
+				<div class="${properties.kcLabelWrapperClass!}">
+					<label for="method-sms" class="${properties.kcLabelClass!}">SMS</label>
+				</div>
+				<div class="${properties.kcInputWrapperClass!}">
+					<input type="radio" id="method-sms" name="method" class="${properties.kcInputClass!}"/>
+				</div>
+
+				<div class="${properties.kcLabelWrapperClass!}">
+					<label for="method-app" class="${properties.kcLabelClass!}">Authenticator App (Google Authenticator, Free Authenticator)</label>
+				</div>
+				<div class="${properties.kcInputWrapperClass!}">
+					<input type="radio" id="method-app" name="method" class="${properties.kcInputClass!}"/>
+				</div>
+			</div>
+			<div class="${properties.kcFormGroupClass!} ${properties.kcFormSettingClass!}">
+				<div id="kc-form-options" class="${properties.kcFormOptionsClass!}">
+					<div class="${properties.kcFormOptionsWrapperClass!}">
+						<span><a href="${url.loginUrl}">${kcSanitize(msg("backToLogin"))?no_esc}</a></span>
+					</div>
+				</div>
+
+				<div id="kc-form-buttons" class="${properties.kcFormButtonsClass!}">
+					<input class="${properties.kcButtonClass!} ${properties.kcButtonPrimaryClass!} ${properties.kcButtonBlockClass!} ${properties.kcButtonLargeClass!}" type="submit" value="${msg("doSubmit")}"/>
+				</div>
+			</div>
+		</form>
+	<#elseif section = "info" >
+		${msg("multipleEnrollmentAuthInstruction")}
+	</#if>
